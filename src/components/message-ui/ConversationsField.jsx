94de4efc296c8c8157511bb630e013cf68f3c63a
robiulalamdev <robiulalam762400@gmail.com>
2023-11-10 00:57:18 +0600

    messages page done

--- src/components/message-ui/ConversationsField.jsx
+++ src/components/message-ui/ConversationsField.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import SingleMessage from "./SingleMessage";
 
 const ConversationsField = () => {
-  return <div className="flex-grow bg-white">dfsfswf</div>;
+  return (
+    <div
+      className="flex-grow h-100 p-2 d-flex flex-column justify-content-end"
+      style={{ overflowY: "auto" }}
+    >
+      <SingleMessage />
+    </div>
+  );
 };
 
 export default ConversationsField;
---
+++ src/components/message-ui/SingleMessage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import img from "../../assets/images/message/chatUser.png";
+
+const SingleMessage = () => {
+  return (
+    <>
+      <div className="single-msg-container">
+        <img src={img} alt="" />
+        <div className="single-msg">
+          <p>I think WoW reported the most concurrent players this year</p>
+        </div>
+      </div>
+      <div className="single-msg-container mt-4">
+        <img src={img} alt="" />
+        <div className="single-msg-dark">
+          <p>I think WoW reported the most concurrent players this year</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleMessage;
