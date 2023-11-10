a55f9961d78061f81120f9be0d7b8ecdce0a1a69
robiulalamdev <robiulalam762400@gmail.com>
2023-11-10 23:03:58 +0600

    media center page done

--- src/components/medial-center-ui/MediaCenterContentArea.jsx
+++ src/components/medial-center-ui/MediaCenterContentArea.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import img1 from "../../assets/images/media-center/img1.png";
+import { iEye } from "../../utils/icons/global_icons";
 
 const MediaCenterContentArea = ({ open }) => {
   return (
-    <div className="w-100">
+    <div className="w-100 d-flex flex-column justify-content-between gap-4">
       {open === 0 && (
         <section className="row">
           <div className="col-6 col-md-4 col-lg-3 mc-card ps-0 mb-3">
@@ -26,24 +27,37 @@ const MediaCenterContentArea = ({ open }) => {
       )}
       {open === 1 && (
         <section className="row">
-          <div className="col-6 col-md-4 col-lg-3  mb-3">
-            <div className=" mc-card row">
-              <div className="col-6 p-0">
-                <img className="w-100 h-100" src={img1} alt="" />
+          <div className="col-6 col-md-4 col-lg-3 d-flex flex-column align-items-center gap-2 mb-3 ps-0">
+            <div className="position-relative">
+              <div className="mc-card row">
+                <div className="col-6 p-0">
+                  <img className="w-100 h-100" src={img1} alt="" />
+                </div>
+                <div className="col-6 p-0">
+                  <img className="w-100 h-100" src={img1} alt="" />
+                </div>
+                <div className="col-6 p-0">
+                  <img className="w-100 h-100" src={img1} alt="" />
+                </div>
+                <div className="col-6 p-0">
+                  <img className="w-100 h-100" src={img1} alt="" />
+                </div>
               </div>
-              <div className="col-6 p-0">
-                <img className="w-100 h-100" src={img1} alt="" />
-              </div>
-              <div className="col-6 p-0">
-                <img className="w-100 h-100" src={img1} alt="" />
-              </div>
-              <div className="col-6 p-0">
-                <img className="w-100 h-100" src={img1} alt="" />
-              </div>
+              <button
+                className="position-absolute"
+                style={{ bottom: "5px", right: "0", zIndex: 50 }}
+              >
+                {iEye}
+              </button>
             </div>
+            <button className="collection-card-btn mx-auto">
+              Call of Duty
+            </button>
           </div>
         </section>
       )}
+
+      <button className="view-more-btn mx-auto text-center">View more</button>
     </div>
   );
 };
--- src/components/medial-center-ui/MediaCenterSidebar.jsx
+++ src/components/medial-center-ui/MediaCenterSidebar.jsx
@@ -37,7 +37,49 @@ const MediaCenterSidebar = ({ open }) => {
           </div>
         </>
       )}
-      <button className="cm-sidebar-save-btn">Save</button>
+
+      {open === 1 && (
+        <>
+          <div>
+            <div className="mc-sidebar-remove-container text-center mt-5">
+              <h1>Create New Collection</h1>
+              <button className="mt-3">Create</button>
+            </div>
+            <div className="mc-sidebar-remove-container text-center mt-5">
+              <h1>Rename Collection</h1>
+              <button className="mt-3 px-3">Select Collection</button>
+            </div>
+
+            <hr className="my-3" style={{ border: "1px solid #5A5A5A" }} />
+            <div className="hide-profile-container mt-5">
+              <h1>Hide from profile</h1>
+              <div className="mc-tab-container d-flex justify-content-around align-items-center mx-auto mt-3">
+                {["Visible", "Hidden"]?.map((t, i) => (
+                  <button
+                    onClick={() => setProfileTab(t)}
+                    className={`${
+                      profileTab === t
+                        ? `mc-active-btn ${
+                            profileTab === "Visible"
+                              ? "mc-btn-bg-visible"
+                              : "mc-btn-bg-hide"
+                          }`
+                        : "mc-dactive-btn"
+                    }`}
+                    key={i}
+                  >
+                    {t}
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      <div className="">
+        <button className="cm-sidebar-delete-btn d-block mb-4">Delete</button>
+        <button className="cm-sidebar-save-btn d-block">Save</button>
+      </div>
     </div>
   );
 };
