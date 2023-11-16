85ee52b72161235df5929a1af1220e0948f3e8ae
robiulalamdev <robiulalam762400@gmail.com>
2023-11-16 11:51:11 +0600

    footer responsive

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -4,44 +4,70 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-       <footer>
+      <footer>
         <div className="container">
-            <div className="row gy-2 text-center text-md-start">
-                <div className="col-12 col-md-6">
-                    <p>THE WALLPAPER SOCIETY</p>
-                </div>
-                <div className="col-12 col-md-2">
-                    <ul>
-                        <li>Navigation</li>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">Account</Link></li>
-                        <li><Link to="/">Favorites</Link></li>
-                        <li><Link to="/">Messages</Link></li>
-                        <li><Link to="/">Site Map</Link></li>
-                    </ul>
-                </div>
+          <div className="row gy-2 text-start">
+            <div className="col-12 col-md-6">
+              <p className="footer-title">THE WALLPAPER SOCIETY</p>
+            </div>
+            <div className="col-6 col-md-2 footer-links">
+              <ul>
+                <li>Navigation</li>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/">Account</Link>
+                </li>
+                <li>
+                  <Link to="/">Favorites</Link>
+                </li>
+                <li>
+                  <Link to="/">Messages</Link>
+                </li>
+                <li>
+                  <Link to="/">Site Map</Link>
+                </li>
+              </ul>
+            </div>
 
-                <div className="col-12 col-md-2">
-                    <ul>
-                        <li>The Society</li>
-                        <li><Link to="/">About</Link></li>
-                        <li><Link to="/">Contact</Link></li>
-                        <li><Link to="/">Copyright Information</Link></li>
-                        <li><Link to="/">Socials</Link></li>
-                        <li><Link to="/">Sponsors / Advertisers</Link></li>
-                    </ul>
-                </div>
+            <div className="col-6 col-md-2 footer-links">
+              <ul>
+                <li>The Society</li>
+                <li>
+                  <Link to="/">About</Link>
+                </li>
+                <li>
+                  <Link to="/">Contact</Link>
+                </li>
+                <li>
+                  <Link to="/">Copyright Information</Link>
+                </li>
+                <li>
+                  <Link to="/">Socials</Link>
+                </li>
+                <li>
+                  <Link to="/">Sponsors / Advertisers</Link>
+                </li>
+              </ul>
             </div>
-            <hr/>
-            <div className="footerBottom d-flex flex-column flex-lg-row justify-content-center align-items-center gap-4">
-                <span>© The Wallpapers Society</span>
-                <span><Link to="/">Community Rules</Link></span>
-                <span><Link to="/">Privacy Policy</Link></span>
-                <span><Link to="/">Terms and Conditions</Link></span>
-            
-            </div>
+          </div>
+          <hr />
+          <div className="footerBottom d-flex justify-content-center align-items-center flex-wrap gap-4">
+            <span className="d-none d-md-block">© The Wallpapers Society</span>
+            <span className="d-md-none">The WPS</span>
+            <span>
+              <Link to="/">Community Rules</Link>
+            </span>
+            <span>
+              <Link to="/">Privacy Policy</Link>
+            </span>
+            <span>
+              <Link to="/">Terms and Conditions</Link>
+            </span>
+          </div>
         </div>
-    </footer>
+      </footer>
     </>
   );
 };
