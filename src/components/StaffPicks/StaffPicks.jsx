b9081e249f534cb59b6736482430e3fa0288a6a4
robiulalamdev <robiulalam762400@gmail.com>
2023-12-04 21:49:31 +0600

    home page : staff pick section responsive

--- src/components/StaffPicks/StaffPicks.jsx
+++ src/components/StaffPicks/StaffPicks.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./StaffPicks.css";
-import staff from "../../assets/staff.png";
-import staff2 from "../../assets/staff2.png";
-import staff3 from "../../assets/staff3.png";
 import { Link } from "react-router-dom";
+
+import sp1 from "../../assets/images/home/spImg1.png";
+import sp2 from "../../assets/images/home/spImg2.png";
+import sp3 from "../../assets/images/home/spImg3.png";
 
 const StaffPicks = () => {
   return (
@@ -12,42 +13,21 @@ const StaffPicks = () => {
         <h2 className="my-4 home-section-title">
           The <span style={{ color: "#FDF516" }}>WPS</span> Staff Picks
         </h2>
-        <div className="row gy-2 gy-md-4">
-          <div className="col-6 px-1 px-md-2">
-            <div className="staff" style={{ maxHeight: "535px" }}>
-              <Link to="#">
-                <img
-                  className="img-fluid object-fit-cover"
-                  src={staff}
-                  alt="Img"
-                  style={{ maxHeight: "535px", minHeight: "115px" }}
-                />
-              </Link>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="d-flex flex-column gap-2 gap-md-4">
-              <div className="staff">
-                <Link to="#">
-                  <img
-                    className="img-fluid w-100 object-fit-cover"
-                    src={staff2}
-                    alt="Img"
-                    // style={{ height: "253px" }}
-                  />
-                </Link>
-              </div>
-              <div className="staff">
-                <Link to="#">
-                  <img
-                    className="img-fluid w-100 object-fit-cover"
-                    src={staff3}
-                    alt="Img"
-                    // style={{ height: "253px" }}
-                  />
-                </Link>
-              </div>
-            </div>
+        <div className="staff_picks_container">
+          <Link to="#">
+            <img className="sp_first-img" src={sp1} alt="Img" />
+          </Link>
+          <div className="w-100">
+            <Link to="#">
+              <img
+                className="img-fluid w-100 sp_second-img"
+                src={sp2}
+                alt="Img"
+              />
+            </Link>
+            <Link to="#">
+              <img className="w-100 sp_third_img" src={sp3} alt="Img" />
+            </Link>
           </div>
         </div>
       </div>
